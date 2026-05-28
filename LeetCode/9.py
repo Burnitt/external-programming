fd2f56b728e517e02e8dca5a81589336a2545256
Gurnit Chopra <gurnitc@gmail.com>
2026-05-28 13:37:44 -0400

    Update 9.py

--- LeetCode/9.py
+++ LeetCode/9.py
@@ -1,7 +1,7 @@
 class Solution:
     def isPalindrome(self, x: int) -> bool:
         char = str(x)
-        for i in range (int((len(char)/2))):
+        for i in range (len(char)//2):
             if char[i] != char[-i - 1]:
                 return False
         return True
