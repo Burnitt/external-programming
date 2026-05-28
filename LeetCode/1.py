c9e5f5735c4d332ce1f6b09833b149250842fa86
Gurnit Chopra <gurnitc@gmail.com>
2026-05-28 13:24:57 -0400

    Create 1.py
    Solution to Problem #1

---
+++ LeetCode/1.py
@@ -0,0 +1,8 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        sol = {}
+        for i in range (len(nums)):
+            diff = target - nums[i]
+            if diff in sol:
+                return[i, sol[diff]]
+            sol[nums[i]] = i
